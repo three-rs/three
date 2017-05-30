35b5139ae22c2d28f27c7d09e6093559954c4e3c
Dzmitry Malyshau <kvarkus@gmail.com>
2017-05-29 22:03:32 -0400

    Shapes example

--- examples/shapes.rs
+++ examples/shapes.rs
@@ -8,10 +8,25 @@ fn main() {
     cam.position = three::Position::new(0.0, 0.0, 5.0);
     let mut win = three::Window::new("Three-rs box mesh drawing example", cam);
 
-    let geometry = three::Geometry::new_box(3.0, 2.0, 1.0);
-    let material = three::Material::MeshBasic { color: 0x00ff00 };
-    let mut mesh = win.factory.mesh(geometry, material);
+    let mut mesh = {
+        let geometry = three::Geometry::new_box(3.0, 2.0, 1.0);
+        let material = three::Material::MeshBasic { color: 0x00ff00 };
+        win.factory.mesh(geometry, material)
+    };
+    mesh.transform_mut().disp.x = -2.0;
     win.scene.add(&mesh);
+
+    let mut line = {
+        let geometry = three::Geometry::from_vertices(vec![
+            three::Position::new(-2.0, -1.0, 0.0),
+            three::Position::new(0.0, 1.0, 0.0),
+            three::Position::new(2.0, -1.0, 0.0),
+        ]);
+        let material = three::Material::LineBasic { color: 0x0000ff };
+        win.factory.mesh(geometry, material)
+    };
+    line.transform_mut().disp.x = 2.0;
+    win.scene.add(&line);
 
     let mut angle = cgmath::Rad::zero();
     let speed = 1.5;
