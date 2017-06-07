41ee32bd97360321e4dbaee3f952119364ff0013
Dzmitry Malyshau <kvarkus@gmail.com>
2017-06-07 08:55:47 -0400

    Texturing for basic material

--- data/shaders/basic_ps.glsl
+++ data/shaders/basic_ps.glsl
@@ -1,8 +1,11 @@
 #version 150 core
 #include locals
 
+in vec2 v_TexCoord;
 out vec4 Target0;
 
+uniform sampler2D t_Map;
+
 void main() {
-    Target0 = u_Color;
+    Target0 = u_Color * texture(t_Map, v_TexCoord);
 }
