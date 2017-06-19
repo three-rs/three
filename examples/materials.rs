4de4e8482d8ec3a6291f000829c710b012fcf783
Dzmitry Malyshau <kvarkus@gmail.com>
2017-06-19 18:19:36 -0400

    Native projection types

--- examples/materials.rs
+++ examples/materials.rs
@@ -2,7 +2,7 @@ extern crate three;
 
 fn main() {
     let mut win = three::Window::new("Three-rs materials example", "data/shaders");
-    let mut cam = win.factory.perspective_camera(75.0, 0.0, 1.0, 50.0);
+    let mut cam = win.factory.perspective_camera(75.0, 1.0, 50.0);
     cam.set_position([0.0, 0.0, 10.0]);
 
     let mut light = win.factory.point_light(0xffffff, 0.5);
