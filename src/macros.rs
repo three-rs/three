719a0c8a89965d7ecc7163ed98e73e7812b6aecb
Sunjay Varma <varma.sunjay@gmail.com>
2017-06-24 00:44:02 -0400

    Added macro for implementing conversion traits on structs that wrap three-rs types.
    Really wanted to replace macros in `src/scene.rs` with some macros that
    could be used outside of the crate, but unfortunately that didn't workout
    because while internal three objects have access to the `node` field,
    structs external to the crate do not. This causes problems in `as_ref` and
    if that one was going to deviate, we might as well make a single macro for
    the special purpose of classes that wrap three-rs objects.

---
+++ src/macros.rs
@@ -0,0 +1,44 @@
+/// Implements conversion traits on a type wrapping a three-rs type. Useful for when you wrap a
+/// three-rs type with your own struct. Allows you to use that struct in place of any three-rs
+/// object.
+///
+/// Implements the following traits:
+///
+/// * `AsRef<NodePointer>`
+/// * `Deref<Target=Object>`
+/// * `DerefMut<Object>`
+///
+/// ```
+/// // Implements conversion traits for MyStruct using field `internal_three_type`
+/// three_object_wrapper!(MyStruct#internal_three_type);
+/// // If field is omitted, it defaults to `object`
+/// three_object_wrapper!(MyStruct); // equivalent to three_object_wrapper!(MyStruct#object);
+/// ```
+#[macro_export]
+macro_rules! three_object_wrapper {
+    ($($name:ident),*) => {
+        three_object_wrapper!($($name#object),*);
+    };
+    ($($name:ident # $field:ident),*) => {
+        $(
+            impl AsRef<$crate::NodePointer> for $name {
+                fn as_ref(&self) -> &$crate::NodePointer {
+                    self.$field.as_ref()
+                }
+            }
+
+            impl ::std::ops::Deref for $name {
+                type Target = Object;
+                fn deref(&self) -> &Object {
+                    &self.$field
+                }
+            }
+
+            impl ::std::ops::DerefMut for $name {
+                fn deref_mut(&mut self) -> &mut Object {
+                    &mut self.$field
+                }
+            }
+        )*
+    };
+}
