f41589512e381cdb4d7a792598cdb17af2a858e8
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2017-08-02 13:07:56 +0300

    Make it possible to run examples from anywhere in the source tree
    Now you can call "cargo run --example <example name>" from any directory,
    not only from the root "Cargo.toml" directory.

--- examples/obj.rs
+++ examples/obj.rs
@@ -4,9 +4,12 @@ use std::env;
 
 fn main() {
     let mut args = env::args();
-    let path = args.nth(1).unwrap_or("test_data/car.obj".to_string());
+    let obj_path: String = format!("{}/test_data/car.obj", env!("CARGO_MANIFEST_DIR"));
+    let path = args.nth(1).unwrap_or(obj_path);
 
-    let mut win = three::Window::new("Three-rs obj loading example", "data/shaders").build();
+    let shaders_path: String = format!("{}/data/shaders", env!("CARGO_MANIFEST_DIR"));
+    let shaders_path_str: &str = shaders_path.as_str();
+    let mut win = three::Window::new("Three-rs obj loading example", shaders_path_str).build();
     let cam = win.factory.perspective_camera(60.0, 1.0, 10.0);
     let mut controls = three::OrbitControls::new(&cam, [0.0, 2.0, -5.0], [0.0, 0.0, 0.0]).build();
 
