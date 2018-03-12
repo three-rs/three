101779723975a721ae53a5cdbe8063722359946d
Dzmitry Malyshau <kvarkus@gmail.com>
2018-03-12 12:59:57 -0400

    Updated includedir and cargo version

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ extern crate includedir_codegen;
 const SHADERS: &str = "data/shaders";
 
 fn main() {
-    println!("cargo:rerun-if-changed={}/*.glsl", SHADERS);
     includedir_codegen::start("FILES")
         .dir(SHADERS, includedir_codegen::Compression::Gzip)
         .build("data.rs")
