33e5563737a770d150957203ba9c1a5e19052124
David LeGare <excaliburhissheath@gmail.com>
2018-03-26 18:21:40 -0500

    Improve documentation for GltfScene

--- src/factory/load_gltf.rs
+++ src/factory/load_gltf.rs
@@ -27,32 +27,54 @@ use geometry::{Geometry, Shape};
 use object::{self, Object};
 use super::Factory;
 
-/// The contents of a scene defined in a glTF file, fully insantiated and ready to be added to
-/// a scene and rendered.
+/// A glTF scene that has been instantiated and can be added to a [`Scene`].
 ///
-/// # Notes
+/// Created by instantiating a scene defined in a [`GltfDefinitions`] with
+/// [`Factory::instantiate_gltf_scene`]. A `GltfScene` can be added to a [`Scene`] with
+/// [`Scene::add`].
 ///
-/// The various contents
+/// # Examples
+///
+/// ```no_run
+/// # let mut window = three::Window::new("three-rs");
+/// let definitions = window.factory.load_gltf("my_data.gltf");
+///
+/// let gltf_scene = window.factory.instantiate_gltf_scene(&definitions, 0);
+/// window.scene.add(&gltf_scene);
+/// ```
+///
+/// [`Scene`]: struct.Scene.html
+/// [`Scene::add`]: struct.Scene.html#method.add
+/// [`GltfDefinitions`]: struct.GltfDefinitions.html
+/// [`Factory::instantiate_gltf_scene`]: struct.Factory.html#method.instantiate_gltf_scene
 #[derive(Debug, Clone)]
 pub struct GltfScene {
     /// A group containing all of the root nodes of the scene.
     ///
     /// While the glTF format allows scenes to have an arbitrary number of root nodes, all scene
     /// roots are added to a single root group to make it easier to manipulate the scene as a
-    /// whole. See `roots` for the full list of root nodes for the scene.
+    /// whole. See [`roots`] for the full list of root nodes for the scene.
+    ///
+    /// [`roots`]: #structfield.roots
     pub group: object::Group,
 
     /// The indices of the root nodes of the scene.
     ///
-    /// Each index corresponds to a node in `nodes`.
+    /// Each index corresponds to a node in [`nodes`].
+    ///
+    /// [`nodes`]: #structfield.nodes
     pub roots: Vec<usize>,
 
     /// The nodes that are part of the scene.
     ///
     /// Node instances are stored in a [`HashMap`] where the key is the node's index in the source
     /// [`GltfDefinitions::nodes`]. Note that a [`HashMap`] is used instead of a [`Vec`] because
-    /// the not all nodes in the source file are guaranteed to be used in the scene, and so node
+    /// not all nodes in the source file are guaranteed to be used in the scene, and so node
     /// indices in the scene instance may not be contiguous.
+    ///
+    /// [`HashMap`]: https://doc.rust-lang.org/stable/std/collections/struct.HashMap.html
+    /// [`Vec`]: https://doc.rust-lang.org/stable/std/vec/struct.Vec.html
+    /// [`GltfDefinitions::nodes`]: struct.GltfDefinitions.html#structfield.nodes
     pub nodes: HashMap<usize, GltfNode>,
 }
 
@@ -156,9 +178,13 @@ pub struct GltfDefinitions {
 
 /// A template for a glTF mesh instance.
 ///
-/// Note that a glTF mesh doesn't map directly to three's concept of a [`Mesh`] (see
-/// [`GltfPrimitiveDefinition`] for a more direct analogy). Rather, `GltfMeshDefinition` can be instantated
-/// into a [`Group`] with one or more [`Mesh`] instances added to the group.
+/// Note that a glTF mesh doesn't map directly to three's [`Mesh`] type (see
+/// [`GltfPrimitiveDefinition`] for a more direct analogy). Rather, `GltfMeshDefinition` can
+/// be instantated into a [`Group`] with one or more [`Mesh`] instances added to the group.
+///
+/// [`Mesh`]: struct.Mesh.html
+/// [`GltfPrimitiveDefinition`]: struct.GltfPrimitiveDefinition.html
+/// [`Group`]: struct.Group.html
 #[derive(Debug, Clone)]
 pub struct GltfMeshDefinition {
     /// The name of the mesh template.
