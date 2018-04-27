9e07d0fcf99c2334a9b4f35359ee6ef7bd6fbaf4
David LeGare <excaliburhissheath@gmail.com>
2018-04-26 23:29:55 -0500

    Cleanup comments in all glTF examples

--- examples/gltf-node-animation.rs
+++ examples/gltf-node-animation.rs
@@ -9,12 +9,14 @@ fn main() {
     window.scene.add(&light);
     window.scene.background = three::Background::Color(0xC6F0FF);
 
-    // Load the glTF file.
     let default = concat!(env!("CARGO_MANIFEST_DIR"), "/test_data/BoxAnimated/BoxAnimated.gltf");
     let path = std::env::args().nth(1).unwrap_or(default.into());
+
+    // Load the contents of the glTF files. Scenes loaded from the file are returned as
+    // `Template` objects, which can be used to instantiate the actual objects for rendering.
     let template = window.factory.load_gltf(&path).pop().unwrap();
 
-    // Instantiate the contents of the file.
+    // Instantiate the contents of the template, and then add it to the scene.
     let (instance, animations) = window.factory.instantiate_template(&template);
     window.scene.add(&instance);
 
@@ -24,14 +26,16 @@ fn main() {
         mixer.action(animation);
     }
 
+    // Create a camera with which to render the scene, and control it with the built-in
+    // orbit controller, set to orbit the model.
     let camera = window.factory.perspective_camera(60.0, 0.1 .. 100.0);
-    window.scene.add(&camera);
-
     let mut controls = three::controls::Orbit::builder(&camera)
         .position([3.0, 3.0, 3.0])
         .target([0.0, 1.0, 0.0])
         .build();
 
+    // Run the main loop, updating the camera controller, animations, and rendering the scene
+    // every frame.
     while window.update() && !window.input.hit(three::KEY_ESCAPE) {
         mixer.update(window.input.delta_time());
         controls.update(&window.input);
